3bedf6cfbc030ee69d3f8cfc580455130f806f03
Ana <ana.raquel.conceicao@gmail.com>
2023-11-29 17:59:26 +0000

    image encoder and decoder

--- trab2/image_decoder.cpp
+++ trab2/image_decoder.cpp
@@ -1,124 +1,118 @@
+#include "golomb.h"
 #include <iostream>
 #include <string>
+#include <string.h>
 #include <vector>
-#include "BitStream.h"
-#include "golomb.h"
 #include "opencv2/opencv.hpp"
+#include "opencv2/highgui/highgui.hpp"
+#include "opencv2/imgproc/imgproc.hpp"
+#include "BitStream.h"
 
 using namespace std;
 using namespace cv;
 
-auto predict = [](int a, int b, int c) {
-    int p;
-    if (c >= std::max(a, b)) {
-      p = std::min(a, b);
-    } else if (c <= std::min(a, b)) {
-      p = std::max(a, b);
-    } else {
-      p = a + b - c;
-    }
-    
-    return p;
-};
+int main(int argc, char *argv[]) {
 
-int prediction_residual(int pixel, int prediction) {
-    return pixel - prediction;
-  }
-  
-  void JPEG_LS_predictor(cv::Mat img_in, cv::Mat &erro) {
-    cv::Size s = img_in.size();
-    for (int i = 0; i < s.height; i++) {
-      for (int j = 0; j < s.width; j++) {
-        int a, b, c;
-        
-        int pixel = img_in.at<uchar>(i, j);
-        
-        if (j == 0 && i != 0) {
-          a = 0;
-          b = img_in.at<uchar>(i - 1, j);
-          c = 0;
-        } else if (j != 0 && i == 0) {
-          a = img_in.at<uchar>(i, j - 1);
-          b = 0;
-          c = 0;
-        } else if (j == 0 && i == 0) {
-          a = 0;
-          b = 0;
-          c = 0;
+    auto predict = [](int a, int b, int c){
+        int p;
+        if (c >= std::max(a, b)) {
+          p = std::min(a, b);
+        } else if (c <= std::min(a, b)) {
+          p = std::max(a, b);
         } else {
-          a = img_in.at<uchar>(i, j - 1);
-          b = img_in.at<uchar>(i - 1, j);
-          c = img_in.at<uchar>(i - 1, j - 1);
+          p = a + b - c;
         }
         
-        int prediction = predict(a, b, c);
-        
-        int error = prediction_residual(pixel, prediction);
-        
-        erro.at<uchar>(i, j) = error;
-      }
-    }
+        return p;
+    };
+
+    //start a timer
+    clock_t start = clock();
+
+    if (argc < 3) {
+		cerr << "Usage: " << argv[0] << " <input file> <output file>\n";
+		return 1;
+	}
+
+    BitStream bs (argv[1], false);
+    string output = argv[2];
+
+    int imgwidth = bs.readNBits(16);
+    int imgheight = bs.readNBits(16);
+    int enc = bs.readNBits(32);
+    int msize = bs.readNBits(16); 
+    //cout << "msize:" << msize << endl;
+
+    Mat new_image = Mat::zeros(imgheight, imgwidth, CV_8U);
+    
+    vector<int> m_vector;
+for (int i = 0; i < msize; i++) {
+    int m = bs.readNBits(16); // Read 16 bits for m
+
+    m_vector.push_back(m);
+    //cout << "m:" << m << endl;
 }
 
-int main(int argc, char *argv[]) {
-    if (argc != 3) {
-        cerr << "Usage: " << argv[0] << " <encoded file> <output image>\n";
-        return 1;
-    }
 
-    string encodedFile = argv[1];
-    string outputFile = argv[2];
-
-    BitStream bitStream(encodedFile, false); // Open the encoded file for reading
-
-    // Read image dimensions from the encoded file
-    int imageWidth = static_cast<int>(bitStream.readNBits(32));
-    int imageHeight = static_cast<int>(bitStream.readNBits(32));
     
-
-    Golomb golomb(8);
-    vector<int> decodedValues;
-    while (!bitStream.eof() && decodedValues.size() < imageWidth * imageHeight) {
-        vector<bool> encoded_bits;
-        int b_value = golomb.getB(); // Get 'b' value for decoding
-        for (int i = 0; i < b_value + 1; ++i) {
-            encoded_bits.push_back(bitStream.readBit());
-        }
-        unsigned int index = 0; // Initialize index as unsigned int
-        decodedValues.push_back(golomb.decode(encoded_bits, index)); // Decode using Golomb's decode function
+    vector<int> encoded_values;
+    for (int i = 0; i < enc; i++) {
+        encoded_values.push_back(bs.readBit());
     }
 
-    bitStream.close(); // Close the BitStream after reading
+    string encodedString = "";
+    for(long unsigned int i = 0; i < encoded_values.size(); i++) {
+        encodedString += to_string(encoded_values[i]);
+    }
+    
+    Golomb g;
+    vector<int> decoded;
 
-    Mat reconstructedImage(imageHeight, imageWidth, CV_8U);
+    if (msize == 1) {
+        decoded = g.decode(encodedString, m_vector[0]);
+    } else {
+        decoded = g.decodeMultiple(encodedString, m_vector, imgwidth);
+    }
 
-    int decodedIndex = 0;
-    // Reconstruct the image from decoded prediction errors
-    for (int i = 0; i < reconstructedImage.rows; ++i) {
-        for (int j = 0; j < reconstructedImage.cols; ++j) {
-            int predicted;
-            int reconstructedPixel;
-
-            if (i == 0 || j == 0) {
-                predicted = 0; // Maintain separation at image edges
+    //undo the predictions
+    int pixel_idx = 0;
+    for (int i = 0; i < imgheight; i++) {
+        for (int j = 0; j < imgwidth; j++) {
+            if (i == 0 && j == 0) {
+                //create a new pixel with the decoded values at the current pixel index and add it to the image 
+                new_image.at<uchar>(j, i) = uchar(decoded[pixel_idx]);
+                pixel_idx++;
+            } else if (i == 0) {
+                //if its the first line of the image, use only the previous pixel (to the left)
+                int pixel = new_image.at<uchar>(i, j-1) + decoded[pixel_idx];
+                new_image.at<uchar>(i, j) = uchar(pixel);
+                pixel_idx++;
+            } else if (j == 0) {
+                //if its the first pixel of the line, use only the pixel above
+                int pixel = new_image.at<uchar>(i-1, j) + decoded[pixel_idx];
+                new_image.at<uchar>(i, j) = uchar(pixel);
+                pixel_idx++;
             } else {
-                int a = reconstructedImage.at<uchar>(i, j - 1);
-                int b = reconstructedImage.at<uchar>(i - 1, j);
-                int c = reconstructedImage.at<uchar>(i - 1, j - 1);
-                predicted = predict(a, b, c);
-            }
-
-            if (decodedIndex < decodedValues.size()) {
-                reconstructedPixel = predicted + decodedValues[decodedIndex];
-                reconstructedImage.at<uchar>(i, j) = saturate_cast<uchar>(reconstructedPixel);
-                decodedIndex++;
+                //if its not the first pixel of the image nor the first line, use the 3 pixels to the left, above and to the left top
+                int pixel = int(predict(new_image.at<uchar>(i, j-1), new_image.at<uchar>(i-1, j), new_image.at<uchar>(i-1, j-1))) + decoded[pixel_idx];
+                new_image.at<uchar>(i, j) = uchar(pixel);
+                pixel_idx++;
             }
         }
     }
 
-    imwrite(outputFile, reconstructedImage);
+    cout << "foi aqui?" << endl;
+ 
+    cout << "foi" << endl;
+  
+    //save the image
+    imwrite(output, new_image);
 
-    cout << "Image successfully reconstructed and saved as " << outputFile << endl;
+    //end the timer
+    clock_t end = clock();
+    double elapsed_secs = double(end - start) / CLOCKS_PER_SEC;
+    elapsed_secs = elapsed_secs * 1000;
+    cout << "Execution time: " << elapsed_secs << " ms" << endl;
 
     return 0;
 }
--- trab2/image_encoder.cpp
+++ trab2/image_encoder.cpp
@@ -1,142 +1,162 @@
+#include "golomb.h"
 #include <iostream>
 #include <string>
 #include <string.h>
 #include <vector>
 #include "opencv2/opencv.hpp"
-#include "BitStream.h" 
-#include "golomb.h"
+#include "opencv2/highgui/highgui.hpp"
+#include "opencv2/imgproc/imgproc.hpp"
+#include "BitStream.h"
 
 using namespace std;
 using namespace cv;
 
-auto predict = [](int a, int b, int c) {
-    int p;
-    if (c >= std::max(a, b)) {
-      p = std::min(a, b);
-    } else if (c <= std::min(a, b)) {
-      p = std::max(a, b);
-    } else {
-      p = a + b - c;
-    }
-    
-    return p;
-  };
-  
-  int prediction_residual(int pixel, int prediction) {
-    return pixel - prediction;
-  }
-  
-  void JPEG_LS_predictor(cv::Mat img_in, cv::Mat &erro) {
-    cv::Size s = img_in.size();
-    for (int i = 0; i < s.height; i++) {
-      for (int j = 0; j < s.width; j++) {
-        int a, b, c;
-        
-        int pixel = img_in.at<uchar>(i, j);
-        
-        if (j == 0 && i != 0) {
-          a = 0;
-          b = img_in.at<uchar>(i - 1, j);
-          c = 0;
-        } else if (j != 0 && i == 0) {
-          a = img_in.at<uchar>(i, j - 1);
-          b = 0;
-          c = 0;
-        } else if (j == 0 && i == 0) {
-          a = 0;
-          b = 0;
-          c = 0;
-        } else {
-          a = img_in.at<uchar>(i, j - 1);
-          b = img_in.at<uchar>(i - 1, j);
-          c = img_in.at<uchar>(i - 1, j - 1);
-        }
-        
-        int prediction = predict(a, b, c);
-        
-        int error = prediction_residual(pixel, prediction);
-        
-        erro.at<uchar>(i, j) = error;
-      }
-    }
-  }
-
-
 int main(int argc, char *argv[]) {
 
-    //function to calculate m based on u
-    auto calc_m = [](int u) {
-        //u = alpha / 1 - alpha
+    //function to calculate m based on p
+    auto calc_m = [](int p) {
+        //p = alpha / 1 - alpha
         //m = - (1 / log(alpha))
-        return (int) - (1/log((double) u / (1 + u)));
+        return (int) - (1/log((double) p / (1 + p)));
     };
 
-    if (argc < 3 || argc > 3) {
+    auto predict = [](int a, int b, int c) {
+        int p;
+        if (c >= std::max(a, b)) {
+          p = std::min(a, b);
+        } else if (c <= std::min(a, b)) {
+          p = std::max(a, b);
+        } else {
+          p = a + b - c;
+        }
+        
+        return p;
+    };
+
+    //start a timer
+    clock_t start = clock();
+
+    // check if the number of arguments is correct
+    if (argc < 3) {
         cerr << "Usage: " << argv[0] << " <input file> <output file> \n";
         return 1;
     }
-    
-    string output = argv[2];
-    Mat img = imread(argv[1], IMREAD_GRAYSCALE); // Load image in grayscale
-    
-    // Apply the JPEG-LS predictor function to generate prediction errors
-    Mat predictionErrors(img.rows, img.cols, CV_8U);
-    JPEG_LS_predictor(img, predictionErrors);
 
-    // Check if the image is loaded
+    //output file
+    string output = argv[2];
+    // read the image in grayscale
+    Mat img = imread(argv[1], IMREAD_GRAYSCALE);
+    
+    // check if the image is loaded
     if (img.empty()) {
         cout << "Could not load image: " << argv[1] << endl;
         return -1;
     }
 
-    vector<int> valuesToBeEncoded;
-    vector<int> m_vector;
+    //test image type
+    //int type = img.type();  
+    //cout << "image type: " << type  << endl;
 
-    // For each pixel in the image
-    for (int i = 0; i < img.rows; ++i) {
-        for (int j = 0; j < img.cols; ++j) {
-            int predicted;
-            if (i == 0 || j == 0) {
-                predicted = 0; // Maintain separation at image edges
+    vector<int> m_vector;
+    vector<int> valuesToBeEncoded;
+    
+    int pixel_index = 0;
+
+    // for each pixel in the image
+    for (int i = 0; i < img.rows; i++) {
+        for (int j = 0; j < img.cols; j++) {
+            // get the pixel (i,j)
+            int pixel = img.at<uchar>(i,j);
+
+            //if its the first pixel of the image, do not use prediction
+            if (i == 0 && j == 0) {
+                valuesToBeEncoded.push_back(pixel);
+                pixel_index++;
+            } else if(i==0){
+                //if the pixel is in the first line of the image, use only the pixel to its left
+                int error = pixel - img.at<uchar>(i,j-1);
+                valuesToBeEncoded.push_back(error);
+                pixel_index++;
+            }else if(j==0){
+                //if the pixel is the first of the line, use only the pixel above
+                int error = pixel - img.at<uchar>(i-1,j);
+                valuesToBeEncoded.push_back(error);
+                pixel_index++;
             } else {
-                int a = img.at<uchar>(i, j - 1);
-                int b = img.at<uchar>(i - 1, j);
-                int c = img.at<uchar>(i - 1, j - 1);
-                predicted = predict(a, b, c);
+                //for all other pixels, use 3 pixels (to the left, above and to the top left)
+                int error = pixel - int(predict(img.at<uchar>(i,j-1), img.at<uchar>(i-1,j), img.at<uchar>(i-1,j-1)));
+                valuesToBeEncoded.push_back(error);
+                pixel_index++;
             }
 
-            int error = prediction_residual(img.at<uchar>(i, j), predicted);
-            predictionErrors.at<uchar>(i, j) = error;
-            
-            // Store prediction errors in valuesToBeEncoded vector
-            valuesToBeEncoded.push_back(error);
+            if(pixel_index % img.cols == 0 && pixel_index != 0) {
+                int sum = 0;
+                
+                for(int j = pixel_index - img.cols; j < pixel_index; j++) {
+                    sum += abs(valuesToBeEncoded[j]);
+                }
+                int p = round(sum / img.cols);
 
-            // Calculate 'm' based on prediction errors and store in m_vector
-            int m = calc_m(abs(error));
-            m_vector.push_back(m);
+                int m = calc_m(p);
+
+                if (m < 1) m = 1;
+
+                m_vector.push_back(m);
+            }
         }
     }
-    
-    Golomb golomb(8);
 
-    // Perform Golomb encoding on valuesToBeEncoded vector and write to output file
+    string encodedString = "";
+
+    Golomb g;
+
+    //size = rows * cols
+    int size = img.rows * img.cols;
+
+    int m_index = 0;
+    for (int i = 0; i < size; i++) {
+        if (i % img.cols == 0 && i != 0) m_index++;
+        //cout << "values to be encoded: " << valuesToBeEncoded[i] << endl;
+        
+        encodedString += g.encode(valuesToBeEncoded[i], m_vector[m_index]);
+    }
+
     BitStream bitStream(output, true);
+   
+    bitStream.writeNBits(img.cols, 16); // Write image width (16 bits)
+    bitStream.writeNBits(img.rows, 16); // Write image height (16 bits)
+    bitStream.writeNBits(encodedString.size(), 32); // Write encodedString size (32 bits)
+    bitStream.writeNBits(m_vector.size(), 16); // Write m_vector size (16 bits)
+    
+    
+    // Write m_vector values (16 bits each)
+    for (size_t i = 0; i < m_vector.size(); i++) {
+        bitStream.writeNBits(m_vector[i], 16);
+    }
 
-    // Write image dimensions to the encoded file
-    bitStream.writeNBits(img.cols, 32); // Write image width
-    bitStream.writeNBits(img.rows, 32); // Write image height
-  
-    for (int i = 0; i < valuesToBeEncoded.size(); ++i) {
-        // Golomb encode 'valuesToBeEncoded[i]' using Golomb encoding function
-        vector<bool> encoded_bits = golomb.encode(valuesToBeEncoded[i]);
+    //contains all the image's encoded values
+    vector<int> encoded_bits;
 
-        // Write the encoded bits to the output file using BitStream
-        for (bool bit : encoded_bits) {
-            bitStream.writeBit(bit);
-        }
+    for (long unsigned int i = 0; i < encodedString.length(); i++)
+        encoded_bits.push_back(encodedString[i] - '0');
+
+
+    // Write the encoded bits
+    for (size_t i = 0; i < encoded_bits.size(); i++) {
+        bitStream.writeBit(encoded_bits[i]);
     }
     
-    bitStream.close(); // Close the BitStream after writing
 
+
+    bitStream.close();
+    
+
+    //end the timer
+    clock_t end = clock();
+    double elapsed_secs = double(end - start) / CLOCKS_PER_SEC;
+    elapsed_secs = elapsed_secs * 1000;
+    cout << "Time: " << elapsed_secs << " ms" << endl;
+ 
     return 0;
 }
+
